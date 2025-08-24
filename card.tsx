20a2fe36b21baad2a1b5721f56ef61ded99e24e5
dawson0919 <nbamoment@gmail.com>
2025-08-25 01:07:27 +0800

    Add files via upload

---
+++ card.tsx
@@ -0,0 +1,8 @@
+'use client';
+import * as React from 'react';
+export function Card({ className='', ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={`rounded-2xl border bg-white shadow-sm ${className}`} {...props} />;
+}
+export function CardContent({ className='', ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={`p-4 ${className}`} {...props} />;
+}
